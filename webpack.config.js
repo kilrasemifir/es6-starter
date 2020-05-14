eb13d9018059365e7dd5b8f672e860392dbc4455
Raoux <killian@semifir.com>
2020-05-14 21:00:49 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const NodemonPlugin = require('nodemon-webpack-plugin')
+const path = require('path')
+module.exports = {
+    entry: './src/index.js',
+    'mode':'none',
+    watch:true,
+    output: {
+        filename: 'main.js',
+        path: path.resolve('./dist')
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js?$/,
+                loader:'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new NodemonPlugin({
+            ignore:['node_modules/*'],
+            ext:'js'
+        })
+    ]
+}
