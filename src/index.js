f8312f354c38d0b7aa82193bd6a35420d85d9beb
Raoux <killian@semifir.com>
2020-05-15 16:23:28 +0200

    Clean code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 console.log("hello world!");
 
-export const addition=(...x)=>{
-    return x.reduce((a,b)=>a+b,0)
-}
-
-export const affect = (x,y)=>{
-    return {x:x, y:y}
-}
-
-export function fooError(){
-    throw new Error("Mon message")
-}
