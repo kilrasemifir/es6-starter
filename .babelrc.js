a8d4212e4492d59c29ff030b89fa00bde6ec21f4
Raoux <killian@semifir.com>
2020-05-16 18:00:05 +0200

    fix babel async

---
+++ .babelrc.js
@@ -0,0 +1,10 @@
+{
+    presets:["@babel/preset-env", {
+            "targets": {
+                "esmodules": true
+            }
+        }
+    ]
+ 
+    plugins: ['@babel/plugin-transform-regenerator']
+}
